ad4f148490addd4ea61e767370a79bc73addf590
EkarshaSumajK <Sumaj.E@fixitylearn.com>
2025-05-09 17:08:50 +0530

    refactor(mongo): remove commented-out code for cleaner codebase

--- src/mongo.ts
+++ src/mongo.ts
@@ -1,22 +1,3 @@
-// import { MongoClient, ServerApiVersion } from 'mongodb'
-// const uri = "mongodb+srv://ekarshasumajkotikalapoodi:kvljvnlkeskos@cluster0.8lierja.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri)
-
-// async function connect() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// connect().catch(console.dir);
 import { MongoClient, Db } from 'mongodb';
 import type { IndexDirection, InsertOneResult, InsertManyResult, DeleteResult, UpdateResult, Document } from 'mongodb';
 import 'dotenv/config';
